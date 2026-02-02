08ca834d60a248057c3b335471273707b6898417
Codesign <codesign@MacBookPro.carolina.rr.com>
2026-02-02 12:00:41 -0500

    Remove theme watcher

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full antialiased" suppressHydrationWarning>
-      <body className="flex h-full bg-zinc-50 dark:bg-black">
+      <body className="flex h-full bg-zinc-50">
         <Providers>
           <div className="flex w-full">
             <Layout>{children}</Layout>
