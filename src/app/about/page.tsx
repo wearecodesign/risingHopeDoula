dc5d310622e585cdf6f69bb1336ad1759d2d2b26
Codesign <codesign@MacBookPro.carolina.rr.com>
2026-02-06 21:30:25 -0500

    Image optimization galore since next export doesnt handle it automatically (boooooo)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,16 @@
 import { type Metadata } from 'next'
-import Image from 'next/image'
 import parse from 'html-react-parser';
 import he from 'he';
 
 import { Container } from '@/components/Container'
 import { InstagramIcon, MailIcon } from '@/components/SocialIcons'
 import portraitImage from '~/images/photos/about/image-1.jpg'
+import portraitImage320 from '~/images/photos/about/image-1-320.jpg'
+import portraitImage1024 from '~/images/photos/about/image-1-1024.jpg'
+import portraitImageWebP320 from '~/images/photos/about/image-1-320.webp'
+import portraitImageWebP1024 from '~/images/photos/about/image-1-1024.webp'
+import portraitImageAvif320 from '~/images/photos/about/image-1-320.avif'
+import portraitImageAvif1024 from '~/images/photos/about/image-1-1024.avif'
 import { SocialLink } from '@/components/SocialLink';
 
 import settings from '../../../content/settings.json';
@@ -19,12 +24,19 @@ export default function About() {
       <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
         <div className="lg:pl-20">
           <div className="max-w-xs px-2.5 lg:max-w-none">
-            <Image
-              src={portraitImage}
-              alt=""
-              sizes="(min-width: 1024px) 32rem, 20rem"
-              className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
-            />
+            <picture>
+              <source srcSet={portraitImageAvif1024.src} media="(min-width: 1024px)" type="image/avif" />
+              <source srcSet={portraitImageAvif320.src} media="(max-width: 1023px)" type="image/avif" />
+              <source srcSet={portraitImageWebP1024.src} media="(min-width: 1024px)" type="image/webp" />
+              <source srcSet={portraitImageWebP320.src} media="(max-width: 1023px)" type="image/webp" />
+              <source srcSet={portraitImage1024.src} media="(min-width: 1024px)" type="image/jpeg" />
+              <source srcSet={portraitImage320.src} media="(max-width: 1023px)" type="image/jpeg" />
+              <img
+                src={portraitImage.src}
+                alt=""
+                className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
+              />
+            </picture>
           </div>
         </div>
         <div className="lg:order-first lg:row-span-2">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import clsx from 'clsx'
 import he from 'he';
 
@@ -7,37 +6,82 @@ import {
   InstagramIcon,
   MailIcon,
 } from '@/components/SocialIcons'
-import image1 from '~/images/photos/home/image-1.jpg'
-import image2 from '~/images/photos/home/image-2.jpg'
-import image3 from '~/images/photos/home/image-3.jpg'
-import image4 from '~/images/photos/home/image-4.jpg'
-import image5 from '~/images/photos/home/image-5.jpg'
+
+import image1320 from '~/images/photos/home/image-1-320.jpg'
+import image2320 from '~/images/photos/home/image-2-320.jpg'
+import image3320 from '~/images/photos/home/image-3-320.jpg'
+import image4320 from '~/images/photos/home/image-4-320.jpg'
+import image5320 from '~/images/photos/home/image-5-320.jpg'
+
+import image1WebP320 from '~/images/photos/home/image-1-320.webp'
+import image2WebP320 from '~/images/photos/home/image-2-320.webp'
+import image3WebP320 from '~/images/photos/home/image-3-320.webp'
+import image4WebP320 from '~/images/photos/home/image-4-320.webp'
+import image5WebP320 from '~/images/photos/home/image-5-320.webp'
+
+import image1Avif320 from '~/images/photos/home/image-1-320.avif'
+import image2Avif320 from '~/images/photos/home/image-2-320.avif'
+import image3Avif320 from '~/images/photos/home/image-3-320.avif'
+import image4Avif320 from '~/images/photos/home/image-4-320.avif'
+import image5Avif320 from '~/images/photos/home/image-5-320.avif'
+
+import image1 from '~/images/photos/home/image-1-640.jpg'
+import image2 from '~/images/photos/home/image-2-640.jpg'
+import image3 from '~/images/photos/home/image-3-640.jpg' 
+import image4 from '~/images/photos/home/image-4-640.jpg'
+import image5 from '~/images/photos/home/image-5-640.jpg'
+
+import image1WebP from '~/images/photos/home/image-1-640.webp'
+import image2WebP from '~/images/photos/home/image-2-640.webp'
+import image3WebP from '~/images/photos/home/image-3-640.webp'
+import image4WebP from '~/images/photos/home/image-4-640.webp'
+import image5WebP from '~/images/photos/home/image-5-640.webp'
+
+import image1Avif from '~/images/photos/home/image-1-640.avif'
+import image2Avif from '~/images/photos/home/image-2-640.avif'
+import image3Avif from '~/images/photos/home/image-3-640.avif'
+import image4Avif from '~/images/photos/home/image-4-640.avif'
+import image5Avif from '~/images/photos/home/image-5-640.avif'
 
 import content from '../../content/home.json';
 import settings from '../../content/settings.json';
 import { SocialLink } from '@/components/SocialLink';
 
 function Photos() {
-  let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
+  const rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
+  const images = [
+    {image: image1, image320: image1320, imageWebP: image1WebP, imageWebP320: image1WebP320, imageAvif: image1Avif, imageAvif320: image1Avif320}, 
+    {image: image2, image320: image2320, imageWebP: image2WebP, imageWebP320: image2WebP320, imageAvif: image2Avif, imageAvif320: image2Avif320}, 
+    {image: image3, image320: image3320, imageWebP: image3WebP, imageWebP320: image3WebP320, imageAvif: image3Avif, imageAvif320: image3Avif320}, 
+    {image: image4, image320: image4320, imageWebP: image4WebP, imageWebP320: image4WebP320, imageAvif: image4Avif, imageAvif320: image4Avif320}, 
+    {image: image5, image320: image5320, imageWebP: image5WebP, imageWebP320: image5WebP320, imageAvif: image5Avif, imageAvif320: image5Avif320}
+  ]
 
   return (
     <div className="mt-16 sm:mt-20">
       <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
-        {[image1, image2, image3, image4, image5].map((image, imageIndex) => (
+        {images.map((imageObject, imageIndex) => (
           <div
-            key={image.src}
+            key={imageObject.image.src}
             className={clsx(
               'relative w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800',
               rotations[imageIndex % rotations.length],
             )}
           >
             <div className="aspect-9/10">
-              <Image
-                src={image}
-                alt=""
-                sizes="(min-width: 640px) 18rem, 11rem"
-                className="absolute inset-0 h-full w-full object-cover"
-              />
+              <picture>
+                <source srcSet={imageObject.imageAvif.src} media="(min-width: 640px)" type="image/avif" />
+                <source srcSet={imageObject.imageAvif320.src} media="(max-width: 639px)" type="image/avif" />
+                <source srcSet={imageObject.imageWebP.src} media="(min-width: 640px)" type="image/webp" />
+                <source srcSet={imageObject.imageWebP320.src} media="(max-width: 639px)" type="image/webp" />
+                <source srcSet={imageObject.image.src} media="(min-width: 640px)" type="image/jpeg" />
+                <source srcSet={imageObject.image320.src} media="(max-width: 639px)" type="image/jpeg" />
+                <img
+                  src={imageObject.image.src}
+                  alt=""
+                  className="absolute inset-0 h-full w-full object-cover"
+                />
+              </picture>
             </div>
           </div>
         ))}
